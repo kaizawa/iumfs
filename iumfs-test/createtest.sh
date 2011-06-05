de6eeb7cdf6b5cbdee74a8db86daeea384cb473d
Kazuyoshi Aizawa <admin2@whiteboard.ne.jp>
2011-06-05 13:42:31 +0900

    Added test for mkfile command, which was previously caused system panic within kzero.

--- iumfs-test/createtest.sh
+++ iumfs-test/createtest.sh
@@ -3,15 +3,16 @@
 # if file are created and deleted continualy.
 
 cd /var/tmp/iumfsmnt            
-while :
+cnt=1
+while [ $cnt -lt 50 ]; 
 do
     num=1                           
     while [ $num -lt 30 ];          
     do                              
-    	mkfile ${num}k ${num}k  
 	#dd if=/dev/zero of=${num}k bs=1k count=${num}
-    	ls -l ${num}k           
+    	mkfile ${num}k ${num}k   > /dev/null 2>&1
     	num=`expr ${num} + 1`   
     done                            
     rm /var/tmp/iumfsmnt/* 
+    cnt=`expr ${cnt} + 1` 
 done
